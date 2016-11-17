c1e188d5dfa48cc7866964c253df96f4e3fe2c10
dbenitez <daviberas6@gmail.com>
2016-11-17 11:47:23 +0100

    Toca probar :O 2.0

--- Models/ConsPokemonModel.php
+++ Models/ConsPokemonModel.php
@@ -7,6 +7,6 @@ class ConsPokemonModel
     const TABLE_NAME = "Pokemon";
     const NAME = "Name";
     const NUM = "PokedexNumber";
-    const TYPE1 = "type1";
-    const TYPE2 = "type2";
+    const TYPE1 = "Type1";
+    const TYPE2 = "Type2";
 }
